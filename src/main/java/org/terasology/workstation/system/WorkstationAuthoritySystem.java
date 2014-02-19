473b97fa17a302d0a25889a352c2f10af899b61c
MarcinSc <marcins78@gmail.com>
2014-02-19 10:47:07 +0000

    Ensuring only one process is executing at the same time, all the others are queued and pending checks are made after process finishes executing. This is to avoid process starting, when some other process is moving items from one inventory (or inventory slot) to another.

--- src/main/java/org/terasology/workstation/system/WorkstationAuthoritySystem.java
+++ src/main/java/org/terasology/workstation/system/WorkstationAuthoritySystem.java
@@ -17,6 +17,7 @@ package org.terasology.workstation.system;
 
 import org.terasology.engine.Time;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
@@ -28,12 +29,19 @@ import org.terasology.workstation.component.WorkstationComponent;
 import org.terasology.workstation.component.WorkstationProcessingComponent;
 import org.terasology.workstation.event.OpenWorkstationRequest;
 import org.terasology.workstation.event.WorkstationProcessRequest;
+import org.terasology.workstation.event.WotkstationStateChanged;
+import org.terasology.workstation.process.InvalidProcessException;
 import org.terasology.workstation.process.ProcessPart;
 import org.terasology.workstation.process.WorkstationProcess;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Marcin Sciesinski <marcins78@gmail.com>
@@ -46,30 +54,53 @@ public class WorkstationAuthoritySystem extends BaseComponentSystem {
     @In
     private Time time;
 
+    private boolean executingProcess;
+
+    private Set<EntityRef> pendingWorkstationChecks = new LinkedHashSet<>();
+
     @ReceiveEvent(components = {WorkstationComponent.class})
     public void userActivatesWorkstation(ActivateEvent event, EntityRef entity) {
         entity.send(new OpenWorkstationRequest());
     }
 
     @ReceiveEvent
+    public void machineAdded(OnAddedComponent event, EntityRef entity, WorkstationComponent workstationComponent, WorkstationComponent workstation) {
+        pendingWorkstationChecks.add(entity);
+        startProcessingIfNotExecuting();
+    }
+
+    @ReceiveEvent
+    public void automaticProcessingStateChanged(WotkstationStateChanged event, EntityRef entity, WorkstationComponent workstationComponent, WorkstationComponent workstation) {
+        pendingWorkstationChecks.add(entity);
+        startProcessingIfNotExecuting();
+    }
+
+    @ReceiveEvent
     public void finishProcessing(DelayedActionTriggeredEvent event, EntityRef workstation, WorkstationComponent workstationComp,
                                  WorkstationProcessingComponent workstationProcessing) {
-        String actionId = event.getActionId();
-        if (actionId.equals(WorkstationUtils.WORKSTATION_PROCESSING)) {
-            long gameTime = time.getGameTimeInMs();
-            Map<String, WorkstationProcessingComponent.ProcessDef> processesCopy = new HashMap<>(workstationProcessing.processes);
-            for (Map.Entry<String, WorkstationProcessingComponent.ProcessDef> processes : processesCopy.entrySet()) {
-                WorkstationProcessingComponent.ProcessDef processDef = processes.getValue();
-                if (processDef.processingFinishTime <= gameTime) {
-                    List<ProcessPart> processParts = workstationRegistry.
-                            getWorkstationProcessById(workstationComp.supportedProcessTypes.keySet(), processDef.processingProcessId).
-                            getProcessParts();
-                    WorkstationUtils.finishProcessing(workstation, workstation, processes.getKey(), workstationProcessing,
-                            processParts, processDef.processingResultId);
+        executingProcess = true;
+        try {
+            String actionId = event.getActionId();
+            if (actionId.equals(WorkstationUtils.WORKSTATION_PROCESSING)) {
+                long gameTime = time.getGameTimeInMs();
+                Map<String, WorkstationProcessingComponent.ProcessDef> processesCopy = new HashMap<>(workstationProcessing.processes);
+                for (Map.Entry<String, WorkstationProcessingComponent.ProcessDef> processes : processesCopy.entrySet()) {
+                    WorkstationProcessingComponent.ProcessDef processDef = processes.getValue();
+                    if (processDef.processingFinishTime <= gameTime) {
+                        List<ProcessPart> processParts = workstationRegistry.
+                                getWorkstationProcessById(workstationComp.supportedProcessTypes.keySet(), processDef.processingProcessId).
+                                getProcessParts();
+                        WorkstationUtils.finishProcessing(workstation, workstation, processes.getKey(), workstationProcessing,
+                                processParts, processDef.processingResultId);
+                    }
                 }
-            }
 
-            WorkstationUtils.scheduleWorkstationWakeUpIfNecessary(workstation, time.getGameTimeInMs());
+                WorkstationUtils.scheduleWorkstationWakeUpIfNecessary(workstation, time.getGameTimeInMs());
+
+                processPendingChecks();
+            }
+        } finally {
+            executingProcess = false;
         }
     }
 
@@ -85,7 +116,83 @@ public class WorkstationAuthoritySystem extends BaseComponentSystem {
             WorkstationProcessingComponent workstationProcessing = workstation.getComponent(WorkstationProcessingComponent.class);
             // It's not processing anything, or not processing this type of process
             if (workstationProcessing == null || !workstationProcessing.processes.containsKey(processType)) {
-                WorkstationUtils.startProcessing(event.getInstigator(), workstation, process, processId, resultId, time.getGameTimeInMs());
+                executingProcess = true;
+                try {
+                    WorkstationUtils.startProcessing(event.getInstigator(), workstation, process, processId, resultId, time.getGameTimeInMs());
+                } finally {
+                    executingProcess = false;
+                }
+            }
+        }
+    }
+
+    private void startProcessingIfNotExecuting() {
+        // This is to avoid a new process starting in a middle of some other process executing
+        if (!executingProcess) {
+            executingProcess = true;
+            try {
+                processPendingChecks();
+            } finally {
+                executingProcess = false;
+            }
+        }
+    }
+
+    private void processPendingChecks() {
+        while (!pendingWorkstationChecks.isEmpty()) {
+            EntityRef workstation = extractFirstPendingWorkstation();
+            if (workstation.exists()) {
+                WorkstationComponent workstationComp = workstation.getComponent(WorkstationComponent.class);
+                if (workstationComp != null) {
+                    processIfHasPendingAutomaticProcesses(workstation, workstationComp);
+                }
+            }
+        }
+    }
+
+    private EntityRef extractFirstPendingWorkstation() {
+        Iterator<EntityRef> checksIterator = pendingWorkstationChecks.iterator();
+        EntityRef workstation = checksIterator.next();
+        checksIterator.remove();
+        return workstation;
+    }
+
+    private void processIfHasPendingAutomaticProcesses(EntityRef entity, WorkstationComponent workstation) {
+        Map<String, Boolean> possibleProcesses = new LinkedHashMap<>(workstation.supportedProcessTypes);
+
+        // Filter out those currently processing
+        WorkstationProcessingComponent processing = entity.getComponent(WorkstationProcessingComponent.class);
+        if (processing != null) {
+            for (String processed : processing.processes.keySet()) {
+                possibleProcesses.remove(processed);
+            }
+        }
+
+        // Filter out non-automatic
+        for (Map.Entry<String, Boolean> processDef : workstation.supportedProcessTypes.entrySet()) {
+            if (processDef.getValue() == false) {
+                possibleProcesses.remove(processDef.getKey());
+            }
+        }
+
+        for (WorkstationProcess workstationProcess : workstationRegistry.getWorkstationProcesses(possibleProcesses.keySet())) {
+            if (possibleProcesses.get(workstationProcess.getId())) {
+                try {
+                    Set<String> possibleResultIds = new HashSet<>();
+                    for (ProcessPart processPart : workstationProcess.getProcessParts()) {
+                        Set<String> resultIds = processPart.validate(entity, entity);
+                        if (resultIds != null) {
+                            possibleResultIds.addAll(resultIds);
+                        }
+                    }
+
+                    if (possibleResultIds.size() <= 1) {
+                        String resultId = possibleResultIds.size() == 0 ? null : possibleResultIds.iterator().next();
+                        WorkstationUtils.startProcessing(entity, entity, workstationProcess, workstationProcess.getId(), resultId, time.getGameTimeInMs());
+                    }
+                } catch (InvalidProcessException exp) {
+                    // Ignored - proceed to next process
+                }
             }
         }
     }
